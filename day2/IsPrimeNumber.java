e342cd74c0c24bbfaa15d58ee354bca25e9565bf
Rajakuart <rajakumarloud@gmail.com>
2025-04-03 14:15:46 +0100

    Add files via upload
    please check the amended one. Thanks

--- day2/IsPrimeNumber.java
+++ day2/IsPrimeNumber.java
@@ -1,23 +1,29 @@
+
 package HomeWorkWeek1.day2;
 
 public class IsPrimeNumber {
 
 	public static void main(String[] args) {
-		int number = 5, count = 0;
-		System.out.println("Your given number is " + number);
-		for (int i = 1; i <= number; i++) {
-			if (number % i == 0) {
-				count++;
-			}
-			//String result = (count==2)? "Its prime number":"not a prime number";
-			//System.out.println(result);
-			if (count == 2) {
-				System.out.println("Number " + number + " is a prime number");
-			break;
-			} else {
-				System.out.println("Number" + number + "is not a prime number");
-break;
-			}
+int n=8, count = 0;
+		
+		for (int i=1;i<=n;i++) {
+		if (n%i==0) {
+		count++;
 		}
+		
+		if (count == 2) {
+            System.out.println("Given number " + n + " is a prime number");
+           
+        } 
+        else {
+            System.out.println("Given number " + n + " is not a prime number");
+        }
+break;
+		}
+		//String result = (count==2)? "Its prime number":"not a prime number";
+		//System.out.println(result);
+		
 	}
+	
 }
+
